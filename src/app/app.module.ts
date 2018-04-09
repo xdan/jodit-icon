02fb7ee1d1aa335be7cf52a4f021a9436967c38a
Chupurnov Valeriy <skoder@ya.ru>
2018-04-09 14:10:06 +0500

    Fix icons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
+import { JoditAngularModule } from 'jodit-angular';
+
 @NgModule({
   declarations: [
     MyApp,
@@ -14,6 +16,7 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
+    JoditAngularModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
